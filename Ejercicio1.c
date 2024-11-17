f299389f089144741aec3a1f44636afd2328b4b1
SantiaGOAT14 <136783629+SantiaGOAT14@users.noreply.github.com>
2024-11-16 20:57:52 -0600

    Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 int main ()
 {
-int lista[5] = {10, 8, 5, 8, 7}; // Se declara e inicializa el arreglo unidimensional
-int indice = 0;
-printf("\tLista\n");
-while (indice < 5 ) // Acceso a cada elemento del arreglo unidimensional usando while
-{
-printf("\nCalificación del alumno %d es %d", indice+1, lista[indice]);
-indice += 1; // Sentencia análoga a indice = indice + 1;
-}
+  int lista[5] = {10, 8, 5, 8, 7}; // Se declara e inicializa el arreglo unidimensional
+  int indice = 0;
+  printf("\tLista\n");
+  while (indice < 5 ) // Acceso a cada elemento del arreglo unidimensional usando while
+  {
+    printf("\n Calificación del alumno %d es %d", indice+1, lista[indice]);
+    indice += 1; // Sentencia análoga a indice = indice + 1;
+  }
+
 printf("\n");
 return 0;
+}
